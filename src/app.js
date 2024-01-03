afb193ea37111d9c98378b3b66dc2331d83a51f5
urialevy <47826356+urialevy@users.noreply.github.com>
2024-01-03 12:49:19 +0100

    now with tailwindcss!

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+// let oneNode = document.createElement("h1");
+// oneNode.classList.add("text-6xl", "underline");
+// oneNode.innerHTML = "hello world";
+// document.body.appendChild(oneNode);
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,44 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+    /*,
+    Plugins: [
+      new HtmlWebpackPlugin({
+        template: ".src/index.html",
+      }),
+    ],*/
+  },
+};
+
+/* const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
@@ -31,3 +69,4 @@ module.exports = {
     runtimeChunk: "single",
   },
 };
+*/
